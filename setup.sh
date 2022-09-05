a0f797fea3c3464b3b0344416d6e752847bfdd19
Emilien GUILMINEAU <eguilmineau@c2s.fr>
2022-09-05 22:54:38 +0200

    Ajout de loki et promtail

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
-mkdir -p ${DATAPATH?Variable DATAPATH not set}/{traefik,portainer,grafana,prometheus,alertmanager}/{data,entrypoint,configs,certificates,logs}
+mkdir -p ${DATAPATH?Variable DATAPATH not set}/{traefik,portainer,grafana,prometheus,alertmanager,loki,promtail}/{data,entrypoint,configs,certificates,logs}
 
-cp -r configs/prometheus/* ${DATAPATH?Variable DATAPATH not set}/prometheus/provisioning
-cp -r configs/alertmanager/* ${DATAPATH?Variable DATAPATH not set}/alertmanager/provisioning
-cp -r configs/grafana/* ${DATAPATH?Variable DATAPATH not set}/grafana/provisioning
+yes | cp -rf configs/prometheus/* ${DATAPATH?Variable DATAPATH not set}/prometheus/provisioning
+yes | cp -rf configs/alertmanager/* ${DATAPATH?Variable DATAPATH not set}/alertmanager/provisioning
+yes | cp -rf configs/grafana/* ${DATAPATH?Variable DATAPATH not set}/grafana/provisioning
+yes | cp -rf configs/loki/* ${DATAPATH?Variable DATAPATH not set}/loki/provisioning
+yes | cp -rf configs/promtail/* ${DATAPATH?Variable DATAPATH not set}/promtail/provisioning
