ed1cadcf78b7dbafa9b333f3b1e70720c4135a68
Emilien GUILMINEAU <eguilmineau@c2s.fr>
2022-09-05 22:41:38 +0200

    Correction de la commande de copie

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 mkdir -p ${DATAPATH?Variable DATAPATH not set}/{traefik,portainer,grafana,prometheus,alertmanager}/{data,entrypoint,configs,certificates,logs}
 
-cp -r configs/prometheus ${DATAPATH?Variable DATAPATH not set}/prometheus/provisioning
-cp -r configs/alertmanager ${DATAPATH?Variable DATAPATH not set}/alertmanager/provisioning
-cp -r configs/grafana ${DATAPATH?Variable DATAPATH not set}/grafana/provisioning
+cp -r configs/prometheus/* ${DATAPATH?Variable DATAPATH not set}/prometheus/provisioning
+cp -r configs/alertmanager/* ${DATAPATH?Variable DATAPATH not set}/alertmanager/provisioning
+cp -r configs/grafana/* ${DATAPATH?Variable DATAPATH not set}/grafana/provisioning
