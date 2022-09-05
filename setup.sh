5b5b6f9c5ecf6b492ff4b86ba7d3d645a15c9d78
Emilien GUILMINEAU <eguilmineau@c2s.fr>
2022-09-05 16:48:09 +0200

    Copie des données pour Prometheus et AlertManager lors du setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
-mkdir -p ${DATAPATH?Variable DATAPATH not set}/{traefik,portainer,grafana}/{data,entrypoint,configs,certificates,logs}
+mkdir -p ${DATAPATH?Variable DATAPATH not set}/{traefik,portainer,grafana,prometheus,alertmanager}/{data,entrypoint,configs,certificates,logs}
 
+cp -r configs/prometheus ${DATAPATH?Variable DATAPATH not set}/prometheus/provisioning
+cp -r configs/alertmanager ${DATAPATH?Variable DATAPATH not set}/alertmanager/provisioning
 cp -r configs/grafana ${DATAPATH?Variable DATAPATH not set}/grafana/provisioning
