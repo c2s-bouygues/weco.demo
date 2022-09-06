06edd2d1cea245e05ac401ee49fccfc1eab4bdbc
Emilien GUILMINEAU <eguilmineau@c2s.fr>
2022-09-06 14:36:40 +0200

    Ajout d'OpenTelemetry Collector et ZipKin

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
-mkdir -p ${DATAPATH?Variable DATAPATH not set}/{traefik,portainer,grafana,prometheus,alertmanager,loki,promtail}/{data,entrypoint,configs,certificates,logs}
+mkdir -p ${DATAPATH?Variable DATAPATH not set}/{traefik,portainer,grafana,prometheus,alertmanager,loki,promtail,otel-collector}/{data,entrypoint,configs,certificates,logs}
 
 yes | cp -rf configs/prometheus/* ${DATAPATH?Variable DATAPATH not set}/prometheus/provisioning
 yes | cp -rf configs/alertmanager/* ${DATAPATH?Variable DATAPATH not set}/alertmanager/provisioning
 yes | cp -rf configs/grafana/* ${DATAPATH?Variable DATAPATH not set}/grafana/provisioning
 yes | cp -rf configs/loki/* ${DATAPATH?Variable DATAPATH not set}/loki/provisioning
 yes | cp -rf configs/promtail/* ${DATAPATH?Variable DATAPATH not set}/promtail/provisioning
+yes | cp -rf configs/otel-collector/* ${DATAPATH?Variable DATAPATH not set}/otel-collector/provisioning
