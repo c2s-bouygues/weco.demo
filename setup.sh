bf3b79b744a882f856b9719281dbf4944784df33
Emilien GUILMINEAU <eguilmineau@c2s.fr>
2022-09-16 16:00:52 +0200

    MIssing InfluxDB scripts

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
-mkdir -p ${DATAPATH?Variable DATAPATH not set}/{traefik,portainer,postgres,grafana,prometheus,alertmanager,loki,promtail,otel-collector,postgres,pulsar,pulsar-manager}/{data,entrypoint,configs,certificates,logs,provisioning}
+mkdir -p ${DATAPATH?Variable DATAPATH not set}/{traefik,portainer,postgres,influxdb,grafana,prometheus,alertmanager,loki,promtail,otel-collector,postgres,pulsar,pulsar-manager}/{data,entrypoint,configs,certificates,logs,provisioning}
 
 envsubst < configs/postgres.sql > ${DATAPATH?Variable DATAPATH not set}/postgres/entrypoint/init.sql
+yes | cp -rf configs/influxdb.sh ${DATAPATH?Variable DATAPATH not set}/influxdb/entrypoint/influxdb.sh
 yes | cp -rf configs/prometheus/* ${DATAPATH?Variable DATAPATH not set}/prometheus/provisioning
 yes | cp -rf configs/alertmanager/* ${DATAPATH?Variable DATAPATH not set}/alertmanager/provisioning
 yes | cp -rf configs/grafana/* ${DATAPATH?Variable DATAPATH not set}/grafana/provisioning
