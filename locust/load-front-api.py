3b1436094e3c867da397427d6bb412662666a98d
Emilien GUILMINEAU <eguilmineau@c2s.fr>
2022-09-20 22:50:14 +0200

    Mise à jour du script Locust

--- locust/load-front-api.py
+++ locust/load-front-api.py
@@ -20,11 +20,11 @@ class WeCoUsers(HttpUser):
         #https://egu-weco-demo-func-planes-ingester.azurewebsites.net/api/swagger/ui?#/planes/SetSpeedMeasure
         #https://egu-weco-demo-func-planes-ingester.azurewebsites.net/api/SetSpeedMeasure?code=Fuqn84rNQOSUy6falT-ZYFDQW_odxMLOUZtp8LYOevziAzFuGL1i7g==
 
-        velocity_types = ["speed", "alt", "dir"]
-        temp_types = ["temp", "temp_min", "temp_max"]
         coords = (self.rnd.random()*2.0, self.rnd.random()*2.0)
         str_1 = ''.join(random.choice(string.ascii_lowercase) for i in range(8))
         str_2 = ''.join((random.choice('0123456789') for i in range(5)))
+        temps = [round(rnd.uniform(0, 25), 2), round(rnd.uniform(0, 25), 2), round(rnd.uniform(0, 25), 2)]
+        temps.sort()
 
         new_speed_measure = {
             "deviceId": str(uuid.uuid4()),
@@ -36,19 +36,47 @@ class WeCoUsers(HttpUser):
             },
             "velocity": [
                 {
-                    "id": self.rnd.randint(0, 999999),
-                    "type": self.rnd.choice(velocity_types),
-                    "value": self.rnd.randint(0, 300),
-                    "unit": "string",
+                    "id": str(uuid.uuid4()),
+                    "type": "speed",
+                    "value": self.rnd.randint(400, 500),
+                    "unit": "knots",
+                    "timestamp": datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
+                },
+                {
+                    "id": str(uuid.uuid4()),
+                    "type": "alt",
+                    "value": self.rnd.randint(9000, 12000),
+                    "unit": "m",
+                    "timestamp": datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
+                },
+                {
+                    "id": str(uuid.uuid4()),
+                    "type": "dir",
+                    "value": self.rnd.randint(0, 359),
+                    "unit": "deg",
                     "timestamp": datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
                 }
             ],
             "temperatures": [
                 {
-                    "id": self.rnd.randint(0, 999999),
-                    "type": self.rnd.choice(temp_types),
-                    "value": self.rnd.randint(0, 50),
-                    "unit": "c",
+                    "id": str(uuid.uuid4()),
+                    "type": "temp",
+                    "value": temps[1],
+                    "unit": "degC",
+                    "timestamp": datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
+                },
+                {
+                    "id": str(uuid.uuid4()),
+                    "type": "temp_min",
+                    "value": temps[0],
+                    "unit": "degC",
+                    "timestamp": datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
+                },
+                {
+                    "id": str(uuid.uuid4()),
+                    "type": "temp_max",
+                    "value": temps[2],
+                    "unit": "degC",
                     "timestamp": datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
                 }
             ]
@@ -65,10 +93,10 @@ class WeCoUsers(HttpUser):
     def setGasMeasure(self):
         #https://egu-weco-demo-func-gas-ingester.azurewebsites.net/api/swagger/ui#/gas/SetGasMeasure
         #https://egu-weco-demo-func-gas-ingester.azurewebsites.net/api/SetGasMeasure?code=B1qYqAfbjN9j9mJXxkmOq0QUbxkzzdtOeoS8BDWyDDAEAzFumGxE6g==
-        gas_types = ["co2", "azote", "oxygen"]
         coords = (self.rnd.random()*2.0, self.rnd.random()*2.0)
         str_1 = ''.join(random.choice(string.ascii_lowercase) for i in range(8))
         str_2 = ''.join((random.choice('0123456789') for i in range(5)))
+        azote_level = round(self.rnd.uniform(10, 30), 2)
 
         new_gas_measure = {
             "deviceId": str(uuid.uuid4()),
@@ -81,9 +109,23 @@ class WeCoUsers(HttpUser):
             "measures": [
                 {
                 "id": str(uuid.uuid4()),
-                "type": self.rnd.choice(gas_types),
-                "value": self.rnd.randint(0, 500),
+                "type": "co2",
+                "value": round(self.rnd.uniform(350, 450), 2),
                 "unit": "ppm",
+                "timestamp": datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
+                },
+                {
+                "id": str(uuid.uuid4()),
+                "type": "azote",
+                "value": azote_level,
+                "unit": "%",
+                "timestamp": datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
+                },
+                {
+                "id": str(uuid.uuid4()),
+                "type": "oxygen",
+                "value": (100 - azote_level),
+                "unit": "%",
                 "timestamp": datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
                 }
             ],
