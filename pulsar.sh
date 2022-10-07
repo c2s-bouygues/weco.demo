1350b51204d60f73c2aa8d4013c3a484be425c32
Emilien GUILMINEAU <mimil.aime.le.pastaga.le.51@gmail.com>
2022-10-07 16:26:01 +0200

    Correction du script d'initialisation de pulsar

--- pulsar.sh
+++ pulsar.sh
@@ -1,7 +1,10 @@
 docker exec -it pulsar bash ./bin/pulsar-admin clusters list
 docker exec -it pulsar bash ./bin/pulsar-admin tenants create iot
-docker exec -it pulsar bash ./bin/pulsar-admin namespaces create iot/incoming
+docker exec -it pulsar bash ./bin/pulsar-admin namespaces create iot/raw
+docker exec -it pulsar bash ./bin/pulsar-admin namespaces create iot/gas
 
+docker exec -it pulsar bash ./bin/pulsar-admin topics create persistent://iot/raw/gas
+docker exec -it pulsar bash ./bin/pulsar-admin topics create persistent://iot/raw/planes
 docker exec -it pulsar bash ./bin/pulsar-admin topics create persistent://iot/gas/azote
 docker exec -it pulsar bash ./bin/pulsar-admin topics create persistent://iot/gas/oxygen
 docker exec -it pulsar bash ./bin/pulsar-admin topics create persistent://iot/gas/co2
